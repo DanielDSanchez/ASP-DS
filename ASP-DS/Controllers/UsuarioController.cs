d32cf6db485fc8b592b33086cd062bd6b6f89c9e
DanielDSanchez <ndanisanchez0525@gmail.com>
2021-08-10 10:39:22 -0500

    CRUD Completo tabla Usuario

--- ASP-DS/Controllers/UsuarioController.cs
+++ ASP-DS/Controllers/UsuarioController.cs
@@ -58,5 +58,71 @@ namespace ASP_DS.Controllers
                 return View(findUser);
             }
         }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var findUser = db.usuario.Find(id);
+                    db.usuario.Remove(findUser);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", "Error " + e);
+                return View();
+            }
+
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuario findUser = db.usuario.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", "Error " + e);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult edit(usuario editUser)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    usuario user = db.usuario.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.apellido = editUser.apellido;
+                    user.email = editUser.email;
+                    user.fecha_nacimiento = editUser.fecha_nacimiento;
+                    user.password = editUser.password;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception e)
+            {
+                ModelState.AddModelError("", "Error " + e);
+                return View();
+            }
+        }
     }
 }
